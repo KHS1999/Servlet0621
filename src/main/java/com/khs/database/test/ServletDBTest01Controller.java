1ddab9010f8804d921aa879091de662309d77a0f
DESKTOP-NJKHAHU\user <mile@naver.com>
2022-06-30 21:46:09 +0900

    [김효석]  - 데이터베이스 연습 예제 - 데이터베이스 기본문제 1번풀이

---
+++ src/main/java/com/khs/database/test/ServletDBTest01Controller.java
@@ -0,0 +1,53 @@
+package com.khs.database.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.khs.common.MysqlService;
+@WebServlet("/db/test01")
+public class ServletDBTest01Controller extends HttpServlet{
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/plain");
+		PrintWriter out = response.getWriter();
+		
+		MysqlService mysqlService =  MysqlService.getInstance();
+		mysqlService.connect();
+		
+		String insertQuery = "INSERT INTO `real_estate` (`realtorId`, `address`,`area`,`type`,`pirce`,`rentPrice`,`createdAt`,`updatedAt`)\r\n"
+				+ "VALUE\r\n"
+				+ "(3,'헤라펠리스 101동 5305호',350,'매매',1500000,NULL,now(),now());";
+		
+		int count = mysqlService.update(insertQuery);
+		
+		out.println("행 삽입 개수 :" + count);
+		
+		String selectQuery = "SELECT `address`, `area`,`type` From `real_estate`  ORDER BY `id`DESC LIMIT 10 ;";
+				
+		ResultSet resultSet = mysqlService.select(selectQuery);
+		
+		try {
+		while(resultSet.next()) {
+				String address = resultSet.getString("address");	
+				int area = resultSet.getInt("area");
+				String type = resultSet.getString("type");
+			
+				
+				
+				out.println("매물주소 :" + address + "면적 :" + area + "타입 : " + type);
+		  	}
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+}
+		}}	
+
+
